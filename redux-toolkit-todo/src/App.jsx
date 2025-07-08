4fd88cd847db7512ca9152fae64528b8bf081b2f
Sanket Singh <91627968+Noob-Sanket@users.noreply.github.com>
2025-07-08 12:49:46 +0530

    redux toolkit project completed

--- redux-toolkit-todo/src/App.jsx
+++ redux-toolkit-todo/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-    <h1>Learn about redux toolkit</h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
